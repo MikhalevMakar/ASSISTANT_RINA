529f4609a3f11b1dcb93f2d34f9958044e34a868
MikhalevMakar <mikhalevmakar@mail.ru>
2023-07-27 17:15:52 +0700

    create config corse

---
+++ backend/src/main/java/ru/nsu/sber_portal/ccfit/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package ru.nsu.sber_portal.ccfit.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+@Configuration
+
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+            .cors(cors -> cors.configurationSource(
+                request -> {
+                    CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+                    configuration.addAllowedMethod (HttpMethod.POST);
+                    configuration.addAllowedMethod (HttpMethod.GET);
+                    configuration.addAllowedMethod (HttpMethod.DELETE);
+                    configuration.addAllowedOriginPattern ("*");
+                    return configuration;
+                }
+            ));
+
+        return http.authorizeHttpRequests(auth ->
+            auth.anyRequest().permitAll()).build();
+    }
+
+}
